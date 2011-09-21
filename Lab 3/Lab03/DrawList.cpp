0f5dfff535c84f049388320474e748b81af484f8
worldwidewilly@comcast.net <Willy Weiskopf@.(none)>
2011-09-20 23:52:50 -0600

    Trying hard on three, but things are blowing up. Whats all this about 'this' not being captured

--- Lab 3/Lab03/DrawList.cpp
+++ Lab 3/Lab03/DrawList.cpp
@@ -4,28 +4,88 @@ using namespace std;
 
 namespace CS262{
 	
+	// Constructors and Destructors
+	DrawList::DrawList(size_type initial_capacity) {
+		MyRects = new value_type*[initial_capacity];
+		capacity = initial_capacity;
+		current_index = 0;
+		used = 0;
+		for(size_type i=0; i<initial_capacity; i++) {
+			value_type*[i] = NULL;
+		}
+	}
+
+	DrawList::DrawList(const DrawList& source) {
+		MyRects = new value_type*[source.capacity];
+
+		used = source.used;
+		capacity = source.capacity;
+
+		copy(source.MyRects, source.MyRects + used, MyRects);
+		for(size_type i=0; i<used; i++) {
+			MyRects[i] = source.MyRects[i];
+		}
+	}
+
+	DrawList::~DrawList() {
+		for(size_type i=0; i<capcity; i++) {
+			delete MyRects[i];
+		}
+		delete [] MyRects;
+	}
+
+
 	// Operators
+	DrawList& DrawList::operator =(const DrawList& source) {
+		if (this == &source) return *this;
+
+		if(capacity != source.capacity) {
+			delete [] MyRects;
+			MyRects = new value_type*[source.capacity];
+			capacity = source.capacity;
+		}
+
+		used = source.used;
+		for(size_type i=0; i<used; i++) {
+			MyRects[i] = source.MyRects[i];
+		}
+		return *this;
+	}
 	void DrawList::operator ++() {
 		assert(!end());
 		current_index++;
 	}
 
-	DrawList::value_type DrawList::operator *() const {
+	DrawList::value_type* DrawList::operator *() const {
 		assert(!end()); 
-		return MyRects[current_index];
+		return *MyRects[current_index];
 	}
 
 
 	// Functions
 	void DrawList::insert(const value_type& entry) {
 		assert(used < capacity);
-		MyRects[used] = entry;
+		MyRects[used] = new value_type(entry);
 		used++;
+	}
+
+	void reserve(size_type new_size = 2*used+1) {
+		if(new_size == capacity) return;
+		if(new_size < used) new_size = used;
+
+		value_type* new_array = new *value_type[new_size];
+		for(size_type i=0; i<used; i++) {
+			new_array[i] = MyRects[i];
+		}
+		delete [] MyRects;
+		MyRects = new_array;
+		capacity = new_size;
 	}
 
 	void DrawList::remove_last() {
 		assert(used > 0);
-		used--;
+		MyRects[--used] = NULL;
+		delete MyRects[used];
 	}
 
 	bool DrawList::end() const {
--- Lab 3/Lab03/DrawList.h
+++ Lab 3/Lab03/DrawList.h
@@ -4,6 +4,7 @@
 #pragma once
 #include <iostream>
 #include <cassert>
+#include <cstdlib>
 #include "Color.h"
 #include "Point.h"
 #include "MyRect.h"
@@ -14,25 +15,28 @@ namespace CS262 {
 			// typedefs and constants
 			typedef MyRect value_type;
 			typedef std::size_t size_type;
-			size_type DEFAULT_CAPACITY = 3;
+			static const size_type DEFAULT_CAPACITY = 3;
 
-			//constructors
-			DrawList() : used(0), current_index(0), capacity(DEFAULT_CAPACITY) {}
+			//constructors and destructors
+			DrawList(size_type initial_capacity = DEFAULT_CAPACITY);
+			DrawList(const DrawList& source);
+			~DrawList();
 
 			//operators
+			DrawList& operator =(const DrawList& source);
 			void operator ++();
-			value_type operator *() const;
+			value_type* operator *() const;
 
 			//functions
 			void insert(const value_type& entry);
+			void reserve();
 			void remove_last();
 			size_type size() const {return used;}
 			void begin() {current_index = 0;}
 			bool end() const;
 
 		private:
-			value_type MyRects[CAPACITY];
-			size_type used, current_index;
-			size_type capacity;
+			value_type **MyRects;
+			size_type used, current_index, capacity;
 	};
 }
