4bd8cc8c252eb8959fe176fa1a7e5f9068cb0e74
William H. Weiskopf, IV <worldwidewilly@comcast.net>
2011-09-24 14:19:38 -0600

    Kinda too late to save, but trying to figure out 3 argument problem with MyLine

--- Lab 4/Lab04/MyLine.h
+++ Lab 4/Lab04/MyLine.h
@@ -14,22 +14,14 @@ namespace CS262 {
 		public:
 			// CONSTRUCTORS
 			MyLine(); // initializes all properties of the line to default values
-			MyLine(Point thePoint1, Point thePoint2, Color theColor=Color(0,0,0), bool isFilled=true) : Point1(thePoint1), Point2(thePoint2), color(theColor) {} //Initializes MyLine to given values
 			MyShape* makeClone() const;
 
 			// ACCESSORS
-		
 
 			// MODIFIERS
-			void setPoint1(Point thePoint1) { Point1 = thePoint1; }
-			void setPoint2(Point thePoint2) { Point2 = thePoint2; }
-			void setColor(Color theColor) { color = theColor; }
-			void setFilled(bool isFilled ) { drawFilled = isFilled; }
 
 			// FUNCTIONS
 			void draw(System::Drawing::Graphics^ g) const;
-			// Postcondition: Returns true if x and y values are the same,
-			// false otherwise
 	};
 
 } // end namespace
--- Lab 4/Lab04/MyOval.h
+++ Lab 4/Lab04/MyOval.h
@@ -14,16 +14,12 @@ namespace CS262 {
 		public:
 			// CONSTRUCTORS
 			MyOval() : drawFilled(true) { } // initializes all properties of the oval to default values
-			MyOval(Point thePoint1, Point thePoint2, Color theColor=Color(0,0,0), bool isFilled=true) : Point1(thePoint1), Point2(thePoint2), color(theColor), drawFilled(isFilled) {} //Initializes MyOval to given values
+			MyOval(Point thePoint1, Point thePoint2, Color theColor=Color(0,0,0), bool isFilled=true) : MyShape(thePoint1, thePoint2, theColor), drawFilled(isFilled) {} //Initializes MyOval to given values
 			MyShape* makeClone() const;
 
 			// ACCESSORS
 		
-
 			// MODIFIERS
-			void setPoint1(Point thePoint1) { Point1 = thePoint1; }
-			void setPoint2(Point thePoint2) { Point2 = thePoint2; }
-			void setColor(Color theColor) { color = theColor; }
 			void setFilled(bool isFilled ) { drawFilled = isFilled; }
 
 			// FUNCTIONS
