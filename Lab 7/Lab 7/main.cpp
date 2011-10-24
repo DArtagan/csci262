c954dd1a311cadb360a0344c74bb2d585de6b25f
William H. Weiskopf, IV <worldwidewilly@comcast.net>
2011-10-24 17:58:27 -0600

    Started Lab 7

---
+++ Lab 7/Lab 7/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+#include "queenSolver.cpp"
+using namespace std;
+
+int main() {
+	// Initialization and Declarations
+	int n(0);
+
+
+	queenSolver(n);
+
+	// Pause
+	system("PAUSE");
+
+	// Exit Program
+	return 0;
+}
---
+++ Lab 7/Lab 7/queenSolver.cpp
@@ -0,0 +1,8 @@
+#include <stack>
+
+std::stack<int> queenSolver(int n) {
+	std::stack<int> queenStack;
+
+
+	return queenStack;
+}
