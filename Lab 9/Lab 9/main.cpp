8f0422a833a809e099d614683f8c449dbad9b4bd
William H. Weiskopf, IV <worldwidewilly@comcast.net>
2011-11-04 12:52:35 -0600

    Lab 9, almost done

---
+++ Lab 9/Lab 9/main.cpp
@@ -0,0 +1,18 @@
+#include "recursion.h"
+
+int main() {
+	while(true) {
+		cin >> h;
+		switch (h) {
+			case '1':
+			   cout << "h is a"; break;
+			case '2':
+			   cout << "h is b"; break;
+			case '3': 
+		}
+	}
+
+	// Pause and exit
+	system("PAUSE");
+	return 0;
+}
---
+++ Lab 9/Lab 9/recursion.h
@@ -0,0 +1,7 @@
+#include <string>
+
+using namespace std;
+
+int gcd(int x, int y);
+double sumover(unsigned int n);
+string reverse(string a, unsigned int lowerBound, unsigned int upBound);
