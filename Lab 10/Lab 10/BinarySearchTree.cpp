113403659ea88c15e5ce30ab459d7b3a6a41cd1d
William H. Weiskopf, IV <worldwidewilly@comcast.net>
2011-12-02 12:49:13 -0700

    I be screwed\!

--- Lab 10/Lab 10/BinarySearchTree.cpp
+++ Lab 10/Lab 10/BinarySearchTree.cpp
@@ -2,20 +2,38 @@
 
 namespace CS262 {
 	template <typename Item, typename Key>
-	template <typename Function>
-	void BinarySearchTree<Item, Key>::preorder(Function m) {
-		return;
-	}
+		BinarySearchTree<Item, Key>::BinarySearchTree() {
+			nodeCount = 0;
+			root = new TreeNode<Item>(Item& data);
+			TreeNode* left = 0;
+			TreeNode* right = 0;
+		}
+	
+	template <typename Item, typename Key>
+		BinarySearchTree<Item, Key>::bool operator <(const Key& L, const Item& R) { 
+			return L < R.name;
+		}
+	
+	template <typename Item, typename Key>
+		BinarySearchTree<Item, Key>::bool operator >(const Key& L, const Item& R) { 
+			return L > R.name;
+		}
 
 	template <typename Item, typename Key>
-	template <typename Function>
-	void BinarySearchTree<Item, Key>::inorder(Function m) {
-		return;
-	}
+		template <typename Function>
+			void BinarySearchTree<Item, Key>::preorder(Function m) {
+				return;
+			}
 
 	template <typename Item, typename Key>
-	template <typename Function>
-	void BinarySearchTree<Item, Key>::postorder(Function m) {
-		return;
-	}
+		template <typename Function>
+			void BinarySearchTree<Item, Key>::inorder(Function m) {
+				return;
+			}
+
+	template <typename Item, typename Key>
+		template <typename Function>
+			void BinarySearchTree<Item, Key>::postorder(Function m) {
+				return;
+			}
 }
--- Lab 10/Lab 10/BinarySearchTree.h
+++ Lab 10/Lab 10/BinarySearchTree.h
@@ -16,6 +16,9 @@ namespace CS262 {
 			~BinarySearchTree(void);
 			BinarySearchTree(const BinarySearchTree& source);
 
+			bool operator <(const Key& L, const Item& R);
+			bool operator >(const Key& L, const Item& L);
+
 			std::size_t size() const;
 			std::size_t height() const;
 			
@@ -34,6 +37,11 @@ namespace CS262 {
 				void inorder(Function f);
 			template <typename Function>
 				void postorder(Function f);
+
+		private:
+			TreeNode<Item>* root;
+			std::size_t nodeCount;
+			//Matt says consider keeping track of height
 	};
 }
 
