3762f58b1463ea63a68e72fe52259bdb2439f826
William H. Weiskopf, IV <worldwidewilly@comcast.net>
2011-12-06 20:33:43 -0700

    Starting Lab 11

---
+++ Lab 11/Lab 11/sort.cpp
@@ -0,0 +1,21 @@
+namespace CS262 {
+	template <typename Item, typename SizeType>
+	void selection_sort(Item array[], SizeType size) {
+
+	}
+
+	template <typename Item, typename SizeType>
+	void insertion_sort(Item array[], SizeType size) {
+
+	}
+
+	template <typename Item, typename SizeType>
+	void merge_sort(Item array[], SizeType size) {
+
+	}
+
+	template <typename Item, typename SizeType>
+	void heap_sort(Item array[], SizeType size) {
+
+	}
+}
---
+++ Lab 11/Lab 11/sort.h
@@ -0,0 +1,17 @@
+#include <algorithm>
+
+namespace CS262 {
+	template <typename Item, typename SizeType>
+	void selection_sort(Item array[], SizeType size);
+
+	template <typename Item, typename SizeType>
+	void insertion_sort(Item array[], SizeType size);
+
+	template <typename Item, typename SizeType>
+	void merge_sort(Item array[], SizeType size);
+
+	template <typename Item, typename SizeType>
+	void heap_sort(Item array[], SizeType size);
+}
+
+#include "sort.cpp"
